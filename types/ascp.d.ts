77270680a3a51ca03a5d21398d9b405c392d7c66
TheNorthMemory <thenorthmemory@dingtalk.com>
2023-02-26 18:05:49 +0800

    types/ascp.d.ts

---
+++ types/ascp.d.ts
@@ -0,0 +1,10 @@
+/** 天猫服务 */
+declare namespace Ascp.Insdustry {
+  /** 送货入户并安装取消询价接口 */
+  interface CancelInquiry {
+    /** 取消原因 */
+    reason: string;
+    /** 服务单号 */
+    wds_coordination_order_id: string;
+  }
+}
